1a3cd87e8f6b893a2b15f69cb36843b056853e01
bryunx <bjlfrey@gmail.com>
2018-09-29 13:16:26 -0700

    assignment3

---
+++ assignment_2/js/first.js
@@ -0,0 +1,11 @@
+// function process(){
+// 	var number1, number2, n1, n2, sum;
+
+// 	number1 = document.forms["myform"].elements["num1"].value;
+// 	number2 = document.forms["myform"].elements["num2"].value;
+
+// 	n1 = parseInt(number1);
+// 	n2 = parseInt(number2);
+// 	sum = n1 + n2;
+// 	document.forms["myform"].elements["result"].value = ("The inputted numbers were " + n1 + ", " + n2 + "\nThe sum of the integers was " + sum);
+// };
