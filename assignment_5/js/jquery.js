1b775e95763c4679527ef90903f5b66535de0d99
bryunx <bjlfrey@gmail.com>
2018-11-23 14:47:49 -0800

    new assignments

---
+++ assignment_5/js/jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("button").click(function(){
+    	$("#text").fadeTo("slow", 0.15);
+        $("#div1").fadeTo("slow", 0.15);
+        $("#div2").fadeTo("slow", 0.4);
+        $("#div3").fadeTo("slow", 0.7);
+    });
+});
